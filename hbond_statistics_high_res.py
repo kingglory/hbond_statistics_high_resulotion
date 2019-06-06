4547659e7d58738834f9785f59bda2f4e9c6bb04
kingglory <312775704@qq.com>
2019-06-06 19:24:19 +0800

    just keep protein

--- hbond_statistics_high_res.py
+++ hbond_statistics_high_res.py
@@ -1,18 +1,22 @@
-from __future__ import division,absolute_import,print_function
+from __future__ import division
 import iotbx.pdb
 import os
-import mmtbx.model
-from libtbx import easy_pickle
 import mmtbx.nci.hbond as hbond
 import mmtbx.model
 from libtbx.utils import null_out
+from libtbx import group_args
+from libtbx import easy_pickle
 from mmtbx.utils import run_reduce_with_timeout
 
-def run():
+def run(file_name):
+  #
+  pdb_inp = iotbx.pdb.input(file_name = file_name)
+  hierarchy = pdb_inp.construct_hierarchy()
+  #
   rr = run_reduce_with_timeout(
-    stdin_lines = None,
-    file_name   = "pdb35c8.ent.gz",
-    parameters  = "-oh -his -flip -keep -allalt -pen9999",
+    stdin_lines = hierarchy.as_pdb_string().splitlines(),
+    file_name   = None,#"model.pdb.gz",
+    parameters  = "-oh -his -flip -keep -allalt -pen9999 -",
     override_auto_timeout_with=None)
   pdb_inp = iotbx.pdb.input(source_info = None, lines = rr.stdout_lines)
   hierarchy = pdb_inp.construct_hierarchy()
@@ -27,35 +31,15 @@ def run():
     log              = null_out())
   return hbond.find(model = model)
 
-
-  
-
-
-
-
-
-
 if __name__ == '__main__':
-
-    r = run()
-    dict = {}
-    dict[1] = r.result
-    easy_pickle.dump("1nym.pickle",dict)
-
-    """
-    path ='/home/wangwensong/PDB/pdb/'
-    of = open("".join([path,"INDEX"]),"r")
-    files = ["".join([path,f]).strip() for f in of.readlines()]
-    of.close()
-    i = 0
-    dict = {}
-    for f in files:
-      pdb_code = os.path.basename(f)[3:7]
-      file_name = f
-      print (file_name)
-      r = run(file_name)
-      dict[pdb_code] = r.result
-    easy_pickle.dump("high_res_hbond.pickle",dict)
-    """
-    
-
+  path = '/home/wangwensong/PDB/pdb/'
+  of = open("".join([path,"INDEX"]),"r")
+  files = ["".join([path,f]).strip() for f in of.readlines()]
+  of.close()
+  dict = {}
+  for f in files:
+    pdb_code = os.path.basename(f)[3:7]
+    file_name = f
+    r = run(f)
+    dict[pdb_code] = (r.result)
+  easy_pickle.dump("high_res_hbond.pickle",dict)
